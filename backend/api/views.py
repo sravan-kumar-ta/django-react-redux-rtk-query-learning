b079ed1ad9879b20ada44c478af9140fde049c3d
sravan-kumar-ta <sravan0095@gmail.com>
2024-07-29 23:23:34 +0530

    serializers, view and url are done

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from .models import Project
+from .serializers import ProjectSerializer
+from rest_framework import viewsets
 
-# Create your views here.
+
+class ProjectView(viewsets.ModelViewSet):
+    queryset = Project.objects.all()
+    serializer_class = ProjectSerializer
--- backend/config/urls.py
+++ backend/config/urls.py
@@ -4,8 +4,17 @@ https://docs.djangoproject.com/en/5.0/topics/http/urls/
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
+
+from api.views import ProjectView
+
+route = routers.DefaultRouter()
+route.register("", ProjectView, basename='projectview')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path('api/', include(route.urls)),
+]
+
+urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
