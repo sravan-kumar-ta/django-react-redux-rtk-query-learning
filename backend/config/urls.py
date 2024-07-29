39e47cb703cfa6bac2e8f92f8a2e868b6c177884
sravan-kumar-ta <sravan0095@gmail.com>
2024-07-29 23:02:04 +0530

    model and django config are done

---
+++ backend/config/urls.py
@@ -0,0 +1,11 @@
+"""
+https://docs.djangoproject.com/en/5.0/topics/http/urls/
+"""
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import path
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
